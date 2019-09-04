e81794c4a818419619138fe724386ec1752b91af
saintlyly <lucktao8@163.com>
2019-09-04 17:11:15 +0800

    用户话题列表

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -34,6 +34,11 @@
         <hr>
         <h5><strong>注册于</strong></h5>
         <p>{{ $user->created_at->diffForHumans() }}</p>
+        <ul class="nav nav-tabs">
+          <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta 的话题</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Ta 的回复</a></li>
+        </ul>
+        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
       </div>
     </div>
 
