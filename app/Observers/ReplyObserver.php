38a49d3c85044ea2e79b3e7699843c615c800a9e
saintlyly <lucktao8@163.com>
2019-09-06 15:38:47 +0800

    删除评论

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -16,8 +16,7 @@ class ReplyObserver
     }
     public function created(Reply $reply)
     {
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
+        $reply->topic->updateReplyCount();
 
         // 通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
@@ -25,6 +24,11 @@ class ReplyObserver
     public function updating(Reply $reply)
     {
         //
+    }
+
+    public function deleted(Reply $reply)
+    {
+         $reply->topic->updateReplyCount();
     }
 
 }
